ed47f22dbb5e61a44a34afec1a35223dfd3dd558
go-dima <dima.gonikman@gmail.com>
2025-07-03 16:04:49 +0300

    Support github new PR view

--- src/components/JiraWidget.stories.tsx
+++ src/components/JiraWidget.stories.tsx
@@ -16,9 +16,10 @@ export const Default: Story = {
     mockData: {
       data: {
         summary: "Sample Issue",
-        isLoading: false,
-        error: null,
+        status: "Open",
       },
+      error: null,
+      loading: false,
     },
   },
 };
@@ -27,6 +28,8 @@ export const Loading: Story = {
   args: {
     jiraID: "JIRA-123",
     mockData: {
+      data: null,
+      error: null,
       loading: true,
     },
   },
@@ -36,7 +39,9 @@ export const Error: Story = {
   args: {
     jiraID: "JIRA-123",
     mockData: {
-      error: { message: "Failed to fetch data" },
+      data: null,
+      error: { name: "Error", message: "Failed to fetch data" },
+      loading: false,
     },
   },
 };
--- src/content.ts
+++ src/content.ts
@@ -5,9 +5,19 @@ import { InitAction } from "./shared.types";
 import "./styles/content.css";
 
 const onInit = async () => {
-  const branchElements = Array.from(
-    document.querySelectorAll("span.commit-ref > a > span.css-truncate-target")
+  let branchElements = Array.from(
+    document.querySelectorAll(
+      '[class*="PullRequestHeaderSummary-module__truncateBranch"]'
+    )
   );
+
+  if (branchElements.length === 0) {
+    branchElements = Array.from(
+      document.querySelectorAll(
+        "span.commit-ref > a > span.css-truncate-target"
+      )
+    );
+  }
 
   const branchNames = branchElements.map(
     (element: HTMLElement) => element.innerText
@@ -23,14 +33,50 @@ const onInit = async () => {
   }
 };
 
+const getAnchorElement = (): {
+  anchorElement: HTMLElement;
+  withMarginTop: boolean;
+} => {
+  const anchorClassName_v1 = "gh-header-meta";
+  const anchorClassName_v2 = "prc-PageHeader-Description";
+  let withMarginTop = true;
+
+  let anchorElement = Array.from(
+    document.querySelectorAll(`[class*="${anchorClassName_v2}"]`)
+  ).find((el) => el.className.includes(anchorClassName_v2))
+    ?.children[0] as HTMLElement;
+  if (!anchorElement) {
+    anchorElement = document.getElementsByClassName(
+      anchorClassName_v1
+    )[0] as HTMLElement;
+    withMarginTop = false;
+  }
+  return { anchorElement, withMarginTop };
+};
+
+const getContainerElement = (): Element | undefined => {
+  const containerClassName_v1 = "gh-header";
+  const containerClassName_v2 = "prc-PageHeader-PageHeader";
+
+  let containerElement = Array.from(
+    document.querySelectorAll(`[class*="${containerClassName_v2}"]`)
+  ).find((el) => el.className.includes(containerClassName_v2));
+  if (!containerElement) {
+    containerElement = document.getElementsByClassName(
+      containerClassName_v1
+    )[0];
+  }
+  return containerElement;
+};
+
 const addFooter = (branchName: string) => {
   if (!document) {
     return;
   }
 
   const jiraID = branchName.toUpperCase().match(/KOD-\d+/)?.[0];
-  const anchorElement = document.getElementsByClassName("gh-header-meta")[0];
-  const containerElement = document.getElementsByClassName("gh-header")[0];
+  const { anchorElement, withMarginTop } = getAnchorElement();
+  const containerElement = getContainerElement();
 
   // Check if containerElement already conatins the newElement
   const lookupNewElemnet = containerElement.querySelector(".jira-widget");
@@ -38,7 +84,7 @@ const addFooter = (branchName: string) => {
     return;
   }
 
-  const newElement = createElement();
+  const newElement = createElement(withMarginTop);
 
   // Render the React component
   const root = ReactDOM.createRoot(newElement);
@@ -51,13 +97,15 @@ const addFooter = (branchName: string) => {
   anchorElement.appendChild(newElement);
 };
 
-function createElement(): HTMLDivElement {
+function createElement(withMarginTop: boolean): HTMLDivElement {
   const newElement = document.createElement("div");
   newElement.className = "jira-widget";
   newElement.style.display = "flex";
   newElement.style.alignItems = "center";
-  // newElement.style.justifyContent = "space-around";
-  // newElement.style.marginTop = "4px";
+
+  if (withMarginTop) {
+    newElement.style.marginTop = "4px";
+  }
   return newElement;
 }
 
